f8919393f64c0deb0bf1d094e0cabebce6acccc8
akshit-non <akshit@non.sa>
2021-09-29 16:01:37 +0530

    Contribute form component and integration

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { Card } from "semantic-ui-react";
+import { Card, Grid } from "semantic-ui-react";
 import Layout from "../../components/Layout";
 import Campaign from "../../ethereum/campaign";
 import web3 from "../../ethereum/web3";
+import ContributeForm from "../../components/contributeForm";
 
 class CampaignShow extends Component {
     static async getInitialProps(props) {
@@ -10,6 +11,7 @@ class CampaignShow extends Component {
         const summary = await campaign.methods.getSummary().call();
 
         return {
+            address: props.query.address,
             minimumContribution: summary[0],
             balance: summary[1],
             requestsCount: summary[2],
@@ -57,7 +59,7 @@ class CampaignShow extends Component {
                 meta: 'Campaign balance (ether)',
                 style: { overflowWrap: 'break-word' }
             }
-            
+
         ];
         return <Card.Group items={items}></Card.Group>
     }
@@ -66,7 +68,15 @@ class CampaignShow extends Component {
         return (
             <Layout>
                 <h3>Campaign Show</h3>
-                {this.renderCards()}
+                <Grid>
+                    <Grid.Column width={10}>
+                        {this.renderCards()}
+                    </Grid.Column>
+                    <Grid.Column width={6}>
+                        <ContributeForm address={this.props.address}/>
+                    </Grid.Column>
+                </Grid>
+
             </Layout>
         );
     }
